c814834d0e44cbb21d41c4c165ad33c4d1fcf2fa
Daniel769 <jeam.nieto769@gmail.com>
2024-04-03 22:11:16 -0500

    (backend):Admin CRUD working

---
+++ ValinyApi.js/controllers/administradoresController.js
@@ -0,0 +1,84 @@
+const Administradores = require('../models/Administradores');
+
+module.exports = {
+    register(req, res) {
+        const administrador = req.body;
+        Administradores.create(administrador, (err, data) => {
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Hubo un error con el registro del administrador',
+                    error: err
+                });
+            }
+            return res.status(201).json({
+                success: true,
+                message: 'El registro se realizó correctamente',
+                data: data
+            });
+        });
+    },
+
+    getById(req, res) {
+        const administradorId = req.params.id; // Obtener el ID del parámetro de la URL
+        Administradores.getById(administradorId, (err, data) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Hubo un error al buscar el administrador',
+                    error: err
+                });
+            }
+            if (!data) {
+                return res.status(404).json({
+                    success: false,
+                    message: 'No se encontró ningún administrador con ese ID'
+                });
+            }
+            return res.status(200).json({
+                success: true,
+                message: 'Administrador encontrado',
+                data: data
+            });
+        });
+    },
+    update(req, res) {
+        const administradorId = req.params.id; // Obtener el ID del administrador a actualizar
+        const newData = req.body; // Obtener los nuevos datos del administrador desde el cuerpo de la solicitud
+
+        Administradores.update(administradorId, newData, (err, data) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Hubo un error al actualizar el administrador',
+                    error: err
+                });
+            }
+            return res.status(200).json({
+                success: true,
+                message: 'Administrador actualizado correctamente',
+                data: data
+            });
+        });
+    },
+
+    delete(req, res) {
+        const administradorId = req.params.id; // Obtener el ID del administrador a eliminar
+
+        Administradores.delete(administradorId, (err, data) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Hubo un error al eliminar el administrador',
+                    error: err
+                });
+            }
+            return res.status(200).json({
+                success: true,
+                message: 'Administrador eliminado correctamente',
+                data: data
+            });
+        });
+    }
+
+}
--- ValinyApi.js/server.js
+++ ValinyApi.js/server.js
@@ -4,31 +4,40 @@ const http = require('http');
 const server = http.createServer(app);
 const logger = require('morgan');
 const cors = require('cors');
+
 /**
 * Importar rutas
 */
-const estudiantes = require('./routes/EstudiantesRoutes');
+const estudiantesRoutes = require('./routes/EstudiantesRoutes');
+const administradoresRoutes = require('./routes/AdministradoresRoutes'); // Importar las rutas de administradores
+
 const port = process.env.PORT || 3000; 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.disable('x-powered-by');
 app.set('port', port);
+
 /**
 * LLamando las rutas
 */
-estudiantes(app);
-//direccion ip V4 de la maquina, consultar con ipconfig 
-//mi ip:192.168.1.43
-server.listen(3000, '192.168.1.19' || 'localhost', function() {
- console.log('Aplicación de NodeJS ' + process.pid + ' inicio en el puerto ' + port);
+estudiantesRoutes(app); // Usar las rutas de estudiantes
+administradoresRoutes(app); // Usar las rutas de administradores
+
+// Dirección IP V4 de la máquina, consulta con ipconfig 
+// Mi IP: 192.168.1.43
+server.listen(port, '192.168.1.35' || 'localhost', function() {
+ console.log('Aplicación de NodeJS ' + process.pid + ' inició en el puerto ' + port);
 });
+
 app.get('/', (req, res) => { 
- res.send('Ruta raiz del Backend');
+ res.send('Ruta raíz del Backend');
 });
-//Error handler 
+
+// Manejador de errores 
 app.use((err, req, res, next) => { 
  console.log(err);
  res.status(err.status || 500).send(err.stack);
-}); 
+});
